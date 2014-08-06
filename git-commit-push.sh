c6ebe9304e8825fe86dd77cdbe5ef349b4d553c7
SureshPasula <suresh.pasula@prolifics.com>
2014-08-06 19:07:29 +0530

    The initial shell script file for the commit and push commands

---
+++ git-commit-push.sh
@@ -0,0 +1,5 @@
+git config --global user.email suresh.pasula@prolifics.com
+git config --global user.name spasula 
+git commit -m "BAR Artifact is deployed in to git repository"
+git push ssh://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git
+
