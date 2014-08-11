48740b1aad5c5d9c28f2e4ae4e77f4d26219e6ba
SureshPasula <suresh.pasula@prolifics.com>
2014-08-11 18:26:39 +0530

    Added github repository name with git pull command

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -2,6 +2,5 @@ git config --global user.email suresh.pasula@prolifics.com
 git config --global user.name spasula
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
-git pull
+git pull https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git
 git push https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git
-
