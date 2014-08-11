212fd7007194ab59488a0ced1a2b6d93c17ef13c
SureshPasula <suresh.pasula@prolifics.com>
2014-08-11 17:38:38 +0530

    Removed -A arguement from the add command

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -1,5 +1,6 @@
 git config --global user.email suresh.pasula@prolifics.com
-git config --global user.name spasula 
+git config --global user.name spasula
+git add ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
 git push ssh://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git
 
