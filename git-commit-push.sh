fcc859c804b78a8eb22f73a2f382c226aa967d0a
SureshPasula <suresh.pasula@prolifics.com>
2014-08-11 18:34:02 +0530

    Added master branch to the existing git push command

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -3,4 +3,4 @@ git config --global user.name spasula
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
 git pull https://SureshPasula@github.com/SureshPasula/PBC.git master
-git push https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git
+git push https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git master
