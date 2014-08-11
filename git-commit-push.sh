c2d9f60f006a0edbd9ead878bd42e93c522a2f98
SureshPasula <suresh.pasula@prolifics.com>
2014-08-11 18:53:12 +0530

    Added --rebase attribute to the git pull command

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -2,5 +2,5 @@ git config --global user.email suresh.pasula@prolifics.com
 git config --global user.name spasula
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
-git pull https://SureshPasula@github.com/SureshPasula/PBC.git master
+git pull --rebase
 git push https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git master
