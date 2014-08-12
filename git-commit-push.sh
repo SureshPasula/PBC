92da4af4ad6425727b90ea860cdf6d3afc2a17f5
SureshPasula <suresh.pasula@prolifics.com>
2014-08-12 17:37:03 +0530

    Added config username argument for the script file

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -1,6 +1,5 @@
 git config --global user.email suresh.pasula@prolifics.com
-git config --global user.name spasula
-git config --bool core.bare true
+git config --global user.name SureshPasula
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
 git push origin git@github.com:SureshPasula/PBC.git master
