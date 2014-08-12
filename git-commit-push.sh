7774431e4f140aaa76517016c9a94e79692a2640
SureshPasula <suresh.pasula@prolifics.com>
2014-08-12 17:39:18 +0530

    Changed push command to https protocol

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -2,7 +2,7 @@ git config --global user.email suresh.pasula@prolifics.com
 git config --global user.name SureshPasula
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
-git push origin git@github.com:SureshPasula/PBC.git master
+git push https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git 
 
 
 
