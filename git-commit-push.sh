101121326292944acd1cdbcf566c465d2e7bd919
SureshPasula <suresh.pasula@prolifics.com>
2014-08-12 15:22:10 +0530

    Added the changes to making the git repository as bare repository

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -1,5 +1,6 @@
 git config --global user.email suresh.pasula@prolifics.com
 git config --global user.name spasula
+git config --bool core.bare true
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
 git pull https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git
