7aba7b46fcdec4d5bc675ce9af0676557ab8e9e6
SureshPasula <suresh.pasula@prolifics.com>
2014-08-12 16:46:29 +0530

    Removed git pull command

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -3,8 +3,8 @@ git config --global user.name spasula
 git config --bool core.bare true
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
-git pull https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git
-git push origin HEAD:master 
+git push origin  
+
 
 
 
