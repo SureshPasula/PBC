fd81f05568a1ff19dbcc72e0f79a6b187ab82651
SureshPasula <suresh.pasula@prolifics.com>
2014-08-12 14:09:37 +0530

    Added password to the push command

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -3,4 +3,5 @@ git config --global user.name spasula
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
 git pull https://SureshPasula@github.com/SureshPasula/PBC.git master
-git push ssh://spasula:prolifics1@github.com/SureshPasula/PBC.git 
+git push ssh://git@github.com:SureshPasula:prolifics1/PBC.git 
+
