7c78dd1b6bfcb7f08538be163e90153ce0bc9f92
SureshPasula <suresh.pasula@prolifics.com>
2014-08-12 18:49:08 +0530

    insted of push command url used origin attribute

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -3,4 +3,4 @@ git config --global user.name SureshPasula
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
 git pull git@github.com:SureshPasula/PBC.git master
-git push https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git master
+git push origin HEAD.master 
