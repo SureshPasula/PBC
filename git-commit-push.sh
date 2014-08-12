498ea66830d8888ca16d674e6a8b4be599bfb951
SureshPasula <suresh.pasula@prolifics.com>
2014-08-12 15:05:23 +0530

    Changed the push command url to origin master

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -3,5 +3,5 @@ git config --global user.name spasula
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
 git pull https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git
-git push https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git 
+git push origin master 
 
