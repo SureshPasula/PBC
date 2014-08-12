29ba76d778f2ced5970268de1358baf2167ef9d3
SureshPasula <suresh.pasula@prolifics.com>
2014-08-12 15:00:59 +0530

    Changed the all the ssh related protocols to https protocol

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -2,6 +2,6 @@ git config --global user.email suresh.pasula@prolifics.com
 git config --global user.name spasula
 git add -A ./PBC/scribblesecond.bar 
 git commit -m "BAR Artifact is deployed in to git repository"
-git pull https://SureshPasula@github.com/SureshPasula/PBC.git master
-git push ssh://git@github.com:SureshPasula:prolifics1/PBC.git 
+git pull https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git
+git push https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git 
 
