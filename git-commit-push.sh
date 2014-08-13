2ba1637712e590b3840be77fa35ddc81250c09ec
SureshPasula <suresh.pasula@prolifics.com>
2014-08-13 18:27:30 +0530

    Added git bar file to the repository

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -1,3 +1,3 @@
-git add -A /home/spasula/artifact/scribblesecond.bar
+git add -A scribblesecond.bar
 git commit -m "BAR Artifact is deployed in to git repository"
 git push --repo=$REPOSITORY_URL -f
