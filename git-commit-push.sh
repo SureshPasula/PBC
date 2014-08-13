16963ced579fb600db2a476cb6751e4199080d46
SureshPasula <suresh.pasula@prolifics.com>
2014-08-13 19:53:40 +0530

    Did the changes to get the url,user name ,password from the properties file

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -1,4 +1,4 @@
 git add -A *
 git pull $REPOSITORY_URL master
 git commit -m "BAR Artifact is deployed in to git repository"
-git push --repo=https://$REPOSITORY_USERNAME:$REPOSITORY_PASSWORD@github.com/$REPOSITORY_USERNAME/PBC.git -f
+git push --repo=https://$REPOSITORY_USERNAME:$REPOSITORY_PASSWORD@github.com/PBC.git -f
