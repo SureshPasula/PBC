36f46696d4c96c90ac0bcc07f7411ba1be954937
SureshPasula <suresh.pasula@prolifics.com>
2014-08-13 21:32:30 +0530

    Added url path to push the bar file to the repository

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -1,3 +1,3 @@
 git add -A *
 git commit -m "BAR Artifact is deployed in to git repository"
-git push --repo= $REPOSITORY_URL -f
+git push --repo= https://SureshPasula:prolifics1@github.com/SureshPasula/PBC.git -f
